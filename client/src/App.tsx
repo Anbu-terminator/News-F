5160605c95a7179d851f86b22e5fdf4b8ca8a0b2
Anbu Sivam.B <anbulegend101@gmail.com>
2025-10-07 00:18:04 +0530

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,18 +12,18 @@ import Chatbot from "@/pages/chatbot";
 import TNPSC from "@/pages/tnpsc";
 import Auth from "@/pages/auth";
 
-function AppRouter() {
+function Router() {
   return (
-    <Routes>
+    <Switch>
       <Route path="/" component={Home} />
       <Route path="/summarizer" component={Summarizer} />
       <Route path="/fake-detector" component={FakeNews} />
       <Route path="/chatbot" component={Chatbot} />
       <Route path="/tnpsc" component={TNPSC} />
       <Route path="/auth" component={Auth} />
-      {/* Fallback */}
-      <Route path="*" component={NotFound} />
-    </Routes>
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
@@ -33,7 +33,7 @@ function App() {
       <ThemeProvider defaultTheme="light" storageKey="flashpress-ui-theme">
         <TooltipProvider>
           <Toaster />
-          <AppRouter />
+          <Router />
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
